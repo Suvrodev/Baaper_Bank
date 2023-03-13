87e8ecb84025ed9022a952699e83fe7a2ba7b45a
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-03-14 00:38:44 +0600

    Debug password

--- JS/Registration_dev.js
+++ JS/Registration_dev.js
@@ -20,8 +20,9 @@ Reset.addEventListener('click',function(){
     Confirm_Password.value="";
 })
 
+let Check_Pass_Rules;
 let UserPassword="";
-Password.addEventListener('keyup',function(event){
+Password.addEventListener('keyup', (event)=>{
      UserPassword=event.target.value;
     const TextLength=UserPassword.length;
     console.log(UserPassword);
@@ -29,23 +30,35 @@ Password.addEventListener('keyup',function(event){
     if(TextLength<6){
         PasswordText.style.display='block';
         PasswordText.innerText="Password Should be more than 6 digit";
+        Check_Pass_Rules= false;
     }else{
         PasswordText.style.display='none';
+        Check_Pass_Rules= true;
     }  
 })
 
 let ConfirmPassword;
 Confirm_Password.addEventListener('keyup',function(event){
+   
+   console.log("Password Result: "+Check_Pass_Rules)
    ConfirmPassword=event.target.value;
    console.log(ConfirmPassword);
-   if(ConfirmPassword!==UserPassword){
-      ConfirmPasswordText.style.display='block';
-      console.log("User Password: "+UserPassword);
+   if(Check_Pass_Rules===true){
+        if(ConfirmPassword!==UserPassword){
+            ConfirmPasswordText.style.display='block';
+            console.log("User Password: "+UserPassword);
+            
+        }else{
+        ConfirmPasswordText.style.display='none'; 
+     }
    }else{
-     ConfirmPasswordText.style.display='none';
+      ConfirmPasswordText.style.display='block';
+      ConfirmPasswordText.innerText='Password is not follow our criteria'
    }
+  
 })
 Submit.addEventListener('click',function(){
+    
     const Get1=MyGlobalEmail(Email.value);
     const Get2=MyGlobalPassword(UserPassword);
     console.log("Global Email: "+Get1+" Global Password: "+Get2);
@@ -54,6 +67,7 @@ Submit.addEventListener('click',function(){
    // var aa="Thus is My Data";
     sessionStorage.setItem("ek",Get1);
     sessionStorage.setItem("pk",Get2);
+    alert('Registration successfully')
   //  alert("Email: "+Get1+"Password: "+Get2);
     /////////////////
     window.location.href='index.html';
